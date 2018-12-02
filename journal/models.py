b244353ae3a88df4b7b9e1ca2f1ae7ce51222ba8
aceh40 <assenuma@yahoo.com>
2018-12-02 00:22:05 -0500

    Added string job functionality. Added string and racket list views.

--- journal/models.py
+++ journal/models.py
@@ -10,8 +10,8 @@ class JournalEntry(models.Model):
     """ Used to store entries made by user.
     """
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField(max_length=200, blank=True, null=True)
+    text = models.TextField(blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True,null=False, blank=False)
 
     def __str__(self):
@@ -42,5 +42,64 @@ class WeightEntry(models.Model):
         """ Meta data, in this case we instruct to order list views by due_back.
         """
         ordering = ['-created_date']
+
+
+class TennisString(models.Model):
+    """ List of tennis strings
+    """
+    make = models.CharField(max_length=20, help_text='Enter string maker.')
+    model = models.CharField(max_length=20, help_text='Enter string model.')
+    variation = models.CharField(default='', blank=True, max_length=100, help_text='Enter string model variation or generation.')
+
+    string_type = (
+        ('g', 'Natural Gut'),
+        ('p', 'Polyester'),
+        ('m', 'Multifilament'),
+        ('n', 'Nylon'),
+    )
+    string_gauge = (('15', '15'), ('16', '16'), ('17', '17'), ('18', '18'), ('19', '19'))
+
+    type = models.CharField(choices=string_type, max_length=1, help_text='Type of string')
+    gauge = models.CharField(choices=string_gauge, max_length=2, help_text='Gauge of string')
+    url = models.URLField(null=True, blank=True, help_text="Link to string's page on stringforum.net")
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.make} {self.model} {self.variation} ({self.gauge})'
+
+
+class TennisRacket(models.Model):
+    """ List of tennis strings
+    """
+    make = models.CharField(max_length=20)
+    model = models.CharField(max_length=150)
+    variation = models.CharField(default='', blank=True, max_length=150)
+    head_size = models.IntegerField(null=True, blank=True)
+    length = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=4)
+    strung_weight = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=4)
+    unstrung_weight = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=4)
+    swing_weight = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=5)
+    balance = models.CharField(null=True, blank=True, max_length=20)
+    stiffness = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=5)
+    beam_width = models.CharField(null=True, blank=True, max_length=20)
+    main_strings = models.IntegerField(null=True, blank=True)
+    cross_strings = models.IntegerField(null=True, blank=True)
+    min_tension = models.IntegerField(null=True, blank=True)
+    max_tension = models.IntegerField(null=True, blank=True)
+    mains_skip = models.CharField(null=True, blank=True, max_length=20)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.make} {self.model} {self.variation}'
+
+
+class TennisStringJob(JournalEntry):
+    """ Journal Entry of body weight exercise.
+    """
+    racket = models.ForeignKey(TennisRacket, on_delete=models.SET_NULL, null=True)
+    main_string_id = models.ForeignKey(TennisString, on_delete=models.SET_NULL, null=True, related_name='main_string_id')
+    main_tension = models.DecimalField(null=False, blank=False, decimal_places=1, max_digits=3)
+    cross_string_id = models.ForeignKey(TennisString, on_delete=models.SET_NULL, null=True, related_name='cross_string_id')
+    cross_tension = models.DecimalField(null=False, blank=False, decimal_places=1, max_digits=3)
 
 
--- journal/urls.py
+++ journal/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path('journal/entry/', views.JournalEntryCreate.as_view(), name='journal_entry'),
     path('weight/', views.WeightList.as_view(), name='weight'),
     path('weight/entry/', views.WeightEntryCreate.as_view(), name='weight_entry'),
-
+    path('tennis_rackets/', views.TennisRacketList.as_view(), name='tennis_racket_list'),
+    path('tennis_rackets/<int:pk>/', views.TennisRacketDetail.as_view(), name='tennis_racket'),
+    path('string_job/', views.string_job, name='string_job'),
+    path('string_job_list/', views.StringJobList.as_view(), name='string_job_list'),
 ]
 
